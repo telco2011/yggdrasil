fd9aab118a5a3d9c0890a59ff1655564108638f6
Unknown <davidlopez.david@gmail.com>
2018-01-12 14:40:50 +1000

    Folder refactor

--- src/startup/index.ts
+++ src/startup/index.ts
@@ -2,5 +2,5 @@ import * as dotenv from 'dotenv';
 
 export { dotenv };
 
-export { Startup } from './niflheim';
-export { Bootstrap, IBootstrapRoute } from './muspell';
+export { Startup } from './niflheim/startup';
+export { Bootstrap, IBootstrapRoute } from './muspell/bootstrap';
