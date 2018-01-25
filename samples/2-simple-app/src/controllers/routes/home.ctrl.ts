35e5f7657144714b13aca0d40ebaf8e41e9023b0
telco2011 <davidlopez.david@gmail.com>
2018-01-25 15:39:26 +1000

    Created some samples

---
+++ samples/2-simple-app/src/controllers/routes/home.ctrl.ts
@@ -0,0 +1,31 @@
+/** yggdrasil imports */
+import { Request, Response } from '@yggdrasil/mvc';
+import { FileLogger } from '@yggdrasil/core';
+
+/**
+ * @class HomeCtrl
+ */
+export class HomeCtrl {
+
+  /** HomeCtrl logger */
+  private logger: FileLogger;
+
+  /** Default constructor */
+  constructor() {
+    this.logger = new FileLogger(HomeCtrl.name);
+  }
+
+  /**
+   * Render Home page
+   *
+   * @method home
+   * @param req Request
+   * @param res Response
+   */
+  public home = (req: Request, res: Response) => {
+    this.logger.debug('go to home.');
+
+    res.render('home', { title: 'Home', message: 'Hello there!' });
+  }
+
+}
