32bba46ebe3ed0d236f247eeaab6df4a8131d721
telco2011 <davidlopez.david@gmail.com>
2018-01-26 15:17:11 +1000

    updated gulpfile in samples

--- samples/2-simple-app/gulpfile.js
+++ samples/2-simple-app/gulpfile.js
@@ -1,1 +1,12 @@
-eval(require('fs').readFileSync('./node_modules/@yggdrasil/devs/gulpclass.js').toString());
+var fs = require('fs');
+
+const gulpclass = './node_modules/@yggdrasil/devs/gulpclass.js';
+
+try {
+  if (fs.statSync(gulpclass)) {
+    console.log('Using @yggdrasil gulpclass.');
+    eval(fs.readFileSync(gulpclass).toString());
+  }
+} catch (error) {
+  console.log(error);
+}
