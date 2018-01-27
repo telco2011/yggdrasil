99d34a8965028f26734deeaa8a340d9174aecd6d
telco2011 <davidlopez.david@gmail.com>
2018-01-27 16:03:50 +1000

    update gulpfile

---
+++ src/devs/class.gulp.ts
@@ -0,0 +1,122 @@
+import { Gulpclass, Task, SequenceTask } from 'gulpclass/Decorators';
+import * as gulp from 'gulp';
+import * as ts from 'gulp-typescript';
+import * as gulpTslint from 'gulp-tslint';
+import * as sass from 'gulp-sass';
+import * as nodemon from 'gulp-nodemon';
+
+import * as del from 'del';
+
+@Gulpclass()
+export class YggdrasilGulpfile {
+
+  private tsProject = ts.createProject('tsconfig.json');
+  private tsTestProject = ts.createProject('tsconfig.spec.json');
+
+  @SequenceTask()
+  public start() {
+    return ['build', ['watch', 'nodemon']];
+  }
+
+  @Task('default', ['build'])
+  public defaultTask() {
+    // Default task. Not code is needed.
+  }
+
+  @SequenceTask()
+  public build() {
+      return ['clean', 'copyAssets', 'compile'];
+  }
+
+  @SequenceTask()
+  public copyAssets() {
+    return ['copySass', 'copyJs', 'copyViews', 'copyStatics'];
+  }
+
+  @Task('watch')
+  public watch() {
+    gulp.watch(['src/**/*.ts'], ['compile']);
+    gulp.watch(['src/public/js/**/*.js'], ['copyJs']);
+    gulp.watch(['src/public/scss/**/*.scss'], ['copySass']);
+    gulp.watch(['src/views/**/*.pug', 'src/views/**/*.hbs'], ['copyViews']);
+  }
+
+  @Task()
+  public nodemon() {
+    return nodemon({
+        script: 'dist/ignition.js',
+        tasks: ['watch']
+      });
+  }
+
+  @Task('clean:all', ['clean'])
+  public cleanAll() {
+    return del([
+      './node_modules'
+    ]);
+  }
+
+  @Task()
+  public clean() {
+    return del([
+      './dist',
+      './logs'
+    ]);
+  }
+
+  @Task()
+  public copyStatics() {
+    return gulp.src([
+        'src/public/images/**/*'
+      ]).pipe(gulp.dest('dist/public/images'));
+  }
+
+  @Task()
+  public copyViews() {
+    return gulp.src([
+        'src/views/**/*'
+      ]).pipe(gulp.dest('dist/views'));
+  }
+
+  @Task()
+  public copyJs() {
+    return gulp.src([
+        // Third-party js
+        'node_modules/bootstrap/dist/js/bootstrap.min.js',
+        'node_modules/jquery/dist/jquery.min.js',
+        'node_modules/tether/dist/js/tether.min.js',
+
+        // App js
+        'src/public/js/main.js'
+      ]).pipe(gulp.dest('dist/public/js'));
+  }
+
+  @Task()
+  public copySass() {
+    return gulp.src([
+        // Third-party styles
+        'node_modules/bootstrap/scss/bootstrap.scss',
+
+        // App styles
+        'src/public/scss/*.scss']
+      ).pipe(sass())
+        .pipe(gulp.dest('dist/public/css'));
+  }
+
+  /** Non Testing Zone */
+  @Task('compile')
+  private typescript() {
+    return this.tsProject.src()
+        .pipe(this.tsProject())
+        .js.pipe(gulp.dest('dist'));
+  }
+
+  /** Testing Zone */
+  @Task('compile:test')
+  private typescriptTest() {
+    return this.tsTestProject.src()
+        .pipe(this.tsTestProject())
+        .js.pipe(gulp.dest('dist'));
+  }
+
+}
