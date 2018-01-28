5722ded29f2d8551d3a37764fded2b8a3eaf53cb
telco2011 <davidlopez.david@gmail.com>
2018-01-28 12:24:15 +1000

    typeorm testing

--- src/data/mongoDBRepository.ts
+++ src/data/mongoDBRepository.ts
@@ -1,15 +1,25 @@
-import { Connection, createConnection, getConnection } from 'typeorm';
+import { Connection, MongoEntityManager, createConnection, getConnection, getMongoManager } from 'typeorm';
 
 export class MongoDBRepository {
 
   private connection: Connection;
+  private manager: MongoEntityManager;
 
   public getConnection(): Connection {
     return getConnection();
   }
 
+  public getManager(): MongoEntityManager {
+    return getMongoManager();
+  }
+
   public async createConnection() {
-    this.connection = await createConnection();
+    this.connection = await createConnection({
+        type: "mongodb",
+        host: "localhost",
+        port: 27017,
+        database: "yggdrasil"
+    });
   }
 
 }
