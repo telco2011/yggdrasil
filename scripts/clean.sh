fcd2918cecaa096650c0509401f12f9a6f28fdf4
telco2011 <davidlopez.david@gmail.com>
2018-02-03 13:18:09 +1000

    Updated clean:lib script

---
+++ scripts/clean.sh
@@ -0,0 +1,3 @@
+ #!/bin/bash 
+shopt -s extglob
+rm -rf ./lib/core/!(package.json) && rm -rf ./lib/data/!(package.json) && rm -rf ./lib/devs/!(package.json) && rm -rf ./lib/mvc/!(package.json) && rm -rf ./lib/security/!(package.json) && rm -rf ./lib/testing/!(package.json)
